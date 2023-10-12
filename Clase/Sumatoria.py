79e28b32f2d72540113e47beb187a4f48d2b8e05
Luis Pablo Lopez Iracheta <luisirach54@gmail.com>
2023-10-12 10:42:15 -0600

    Hola

--- Clase/Sumatoria.py
+++ Clase/Sumatoria.py
@@ -4,3 +4,16 @@ for i in range(0, 4, 1):
     print(i)
     s += 3 * (0.5) ** i
 print(f"La suma es: {s}")
+
+#Promedio
+a = [34, 43, 55, 67, 67, 44]
+
+n = len(a)
+
+s = 0
+
+for i in range(0, n):
+    s += a[i]
+mean = (1 / n) * s
+
+print("El promedio es: ", mean)
