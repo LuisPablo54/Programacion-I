f599787801bae8d45310c7e3b3d5b9c0a91ad3d7
Luis Pablo Lopez Iracheta <luisirach54@gmail.com>
2023-11-09 18:58:40 -0600

    Practica retur

--- Clase/Funciones.py
+++ Clase/Funciones.py
@@ -1,5 +1,5 @@
 #Funciones con lisatas
-
+#Todas las funciones tiene que tener un 'return' incluso vacio
 def miLista (x): #Un alias
     x = x * 2
     return x
@@ -10,12 +10,26 @@ def clonandoLista (x): # Un clon
     y[0] = y[0] * 5 #Multiplico el primer valor por 5
     return y
 
+#Valores de return, puedo incluso haber varios valores de return en una función
+def Maximo(a, b):
+    if a > b:
+        resultado = a
+    else:
+        resultado = b
+
+    return resultado #Return es como un brake
+
+    print("Esto no se va a ejecutar")
+    
 
 a = [1, 2, 3, 4]
 y = miLista(a)
 d = clonandoLista(a)
 
+
+
 print("y: ", y)
 print("a: ", a)
 print("d: ", d)
 
+print("El valor maximo es", Maximo(10, 20))
