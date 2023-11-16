26e6297b5d9cdc2cd56da6504a8659362475f82c
Luis Pablo Lopez Iracheta <luisirach54@gmail.com>
2023-11-16 10:02:40 -0600

    Actualizacicion de clases

---
+++ Clase/numeroRamdom.py
@@ -0,0 +1,33 @@
+#Numero ramdom y contarlos
+
+import random
+
+#Genro la lsitas vacia}
+lista = []
+
+for i in range(0, 16):
+    lista.append(0)
+
+for i in range(32):
+    numero =random.randint(1, 15)
+    lista[numero] += 1
+    #print(numero)
+
+#Imprimo la lista
+print("Resultados")
+for i in range (0, 16):
+    print(i, ":", lista[i])
+print("Fin")
+
+#Lista de baraja
+miLista = []
+for i in range (1, 7):
+    miLista.append(i)
+
+print("Lista:", lista)
+
+for posiscipon in range(0, 6):
+    nuevaPos = random.randint(0,5)
+    (lista[posiscipon], lista[nuevaPos]) = (lista[nuevaPos], lista[posiscipon])
+
+print("Lista recuelta: ", lista)
